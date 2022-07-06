b22c3ee243ea7eb713390f28567716eb9e9b71ac
Kristen Prescott <kristennprescott@gmail.com>
2022-07-06 13:12:33 -0400

    set up admin page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  target: "serverless",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
